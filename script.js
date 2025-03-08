74869770234af868cd27e32e31424996a8c765f9
Pallavi-EC144 <pallavikv459@gmail.com>
2025-03-08 18:38:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -99,4 +99,5 @@ function loadPage(page) {
           <div class="grid">
             <div class="sub-tab" onclick="loadPage('ongoing-pregnancy')">
               <h3>Ongoing Pregnancy</h3>
-              <p>
+              <p>Tracking health, doctor consultations, medical alerts.</p>
+            </div
